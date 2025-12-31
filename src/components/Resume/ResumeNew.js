12b201d3d705dc9c28ddee0a85f959bd564586d8
Rao Saqlain King <150324276+RaoSaqlainKing@users.noreply.github.com>
2025-12-31 09:13:02 +0500

    ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,6 +6,9 @@ import pdf from "../../Assets/saqlain-cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -20,19 +23,34 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: "250px" }}>
-            <AiOutlineDownload />&nbsp;Download CV
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
           </Button>
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center" 
-            loading={<div style={{color: "white"}}>Loading PDF…</div>}>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document 
+            file={pdf} 
+            className="d-flex justify-content-center" 
+            loading={<div style={{color: "white"}}>Loading PDFâ€¦</div>}
+          >
+            <Page 
+              pageNumber={1} 
+              scale={width > 786 ? 1.7 : 0.6} 
+              renderTextLayer={false} 
+              renderAnnotationLayer={false}
+            />
           </Document>
         </Row>
       </Container>
     </div>
   );
 }
+
 export default ResumeNew;
